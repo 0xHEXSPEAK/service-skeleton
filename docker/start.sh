65d243e0b463e5aa5a58e9b38cd6b8fdcf4792b6
devyk <hortuk@yahoo.com>
2017-03-11 15:11:44 +0200

    Unused composer dependencies has been removed.

--- docker/start.sh
+++ docker/start.sh
@@ -5,8 +5,6 @@ docker-compose build
 docker-compose up -d
 composer install --prefer-dist --optimize-autoloader --ignore-platform-reqs
 
-# Give a moment for mysql daemon to start working
-sleep 10
 docker-compose run app console/yii app/setup --interactive=0
 
 
